c1abba37e91b7144610c0e310a5d0364d0ffa73a
Elfin Git <musongoletracy@gmail.com>
2023-02-26 14:54:29 +0200

    Create web table for offers and targets

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,26 @@
 class OffersController < ApplicationController
+ # before_action :set_offer only: %i[show update destroy]
+
+  def index
+    @offers = Offer.includes(:offers_targets)
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  def destroy
+    @offer.destroy
+
+    respond_to do |format|
+        format.html { redirect_to offers_url, notice: 'Offer successfully destroyed.' }
+        format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_offer
+    @offer = Offer.find(params[:id])
+  end
 end
